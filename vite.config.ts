fe26aa0a3e977be28479431be2a34d14f3745d17
merzougnaim05-code <merzougnaim05-code@users.noreply.github.com>
2026-09-24 01:31:25 +0200

    ضبط مسار GitHub Pages للنشر

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/SOUMISSION/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
